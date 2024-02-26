14ba86150d3121353eb5b3280c9cbcc4a7b6cb55
Bruno Bechara <Brunosbechara@gmail.com>
2024-02-26 14:36:11 -0300

    Desafio Concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone iph = new Iphone("18 999999999", "Iphone 15 Pro Max", "MEI 1", 14);
+Nokia nok = new Nokia("18 777777777", "Nokia 5200", "MEI 2", 2);
+
+iph.InstalarAplicativo("Instagram");
+Console.WriteLine($"Número do Iphone: {iph.Numero}");
+iph.Ligar();
+
+Console.WriteLine("\n");
+
+nok.InstalarAplicativo("Jogo da cobrinha");
+Console.WriteLine($"Número do Nokia: {nok.Numero}");
+nok.ReceberLigacao();
+
+
+
